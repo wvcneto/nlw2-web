0b1f2893d922fd73d117d756ad0b30cebca001f8
wvcneto <wvcneto@gmail.com>
2020-08-03 11:47:17 -0300

    starting the project

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Landing from './pages/landing';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
